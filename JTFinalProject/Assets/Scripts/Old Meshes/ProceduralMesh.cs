b1e3c3364742ab5ec1b07728d542a9b137b17b96
Detroit Blood <lboo300@gmail.com>
2019-11-11 01:34:35 -0600

    ProcMeshDemo1

---
+++ JTFinalProject/Assets/Scripts/Old Meshes/ProceduralMesh.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class ProceduralMesh : MonoBehaviour
+{
+    Mesh mesh;
+    int[] triangles;
+    Vector3[] veritices;
+    float lastyValue;
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeMeshData();
+        CreateMesh();
+    }
+
+    void MakeMeshData()
+    {
+        //create an array of vertices
+        veritices = new Vector3[] { new Vector3(0,YValue.ins.yValue,0), new Vector3(0,0,1), new Vector3(1,0,0), new Vector3(1, 0, 1) };
+        ///create an array of integers
+        triangles = new int[] { 0, 1, 2,2,1,3};
+    }
+
+    void CreateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = veritices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (YValue.ins.yValue != lastyValue)
+        {
+            MakeMeshData();
+            CreateMesh();
+            lastyValue = YValue.ins.yValue;
+        }
+    }
+}
---
+++ JTFinalProject/Assets/Scripts/ProceduralCube.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class ProceduralCube : MonoBehaviour
+{
+    Mesh mesh;
+    List<Vector3> verticies;
+    List<int> triangles;
+
+    private void Awake()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MakeCube();
+        UpdateMesh();
+    }
+
+    void MakeCube()
+    {
+        verticies = new List<Vector3>();
+        triangles = new List<int>();
+        for (int i = 0; i < 6; i++)
+        {
+            MakeFace(i);
+        }
+    }
+
+    void MakeFace(int dir)
+    {
+        verticies.AddRange(CubeMeshData.faceVerticies(dir));
+        int vCount = verticies.Count;
+        triangles.Add(vCount - 4);
+        triangles.Add(vCount - 4 + 1);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4);
+        triangles.Add(vCount - 4 + 2);
+        triangles.Add(vCount - 4 + 3);
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = verticies.ToArray();
+        mesh.triangles = triangles.ToArray();
+        mesh.RecalculateNormals();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
